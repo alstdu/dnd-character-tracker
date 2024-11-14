b1cf4787163b346d92170539fef0040c0d8641cf
alstdu <lexi.dugo@gmail.com>
2024-11-14 11:42:11 -0800

    feat: Include equipment in inventory weight and value calculations
    Update inventory component to:
    - Add equipment weights to total weight calculation
    - Add equipment values to total value calculation
    - Create helper methods getEquipmentWeight() and getEquipmentValue()
    - Split calculations into inventory and equipment components
    
    This gives a more accurate total of character possessions by including
    both carried inventory and equipped items.

--- dnd-character-tracker/src/app/features/character-sheet/inventory/inventory.component.ts
+++ dnd-character-tracker/src/app/features/character-sheet/inventory/inventory.component.ts
@@ -13,12 +13,66 @@ export class InventoryComponent {
   @Input() character!: Character;
 
   getTotalWeight(): number {
-    return this.character.inventory.reduce((total, item) => 
+    const inventoryWeight = this.character.inventory.reduce((total, item) => 
       total + (item.weight * item.quantity), 0);
+    
+    const equipmentWeight = this.getEquipmentWeight();
+    
+    return inventoryWeight + equipmentWeight;
+  }
+
+  private getEquipmentWeight(): number {
+    let weight = 0;
+    
+    // Add armor weight if equipped
+    if (this.character.equipment.armor) {
+      weight += this.character.equipment.armor.weight;
+    }
+    
+    // Add main hand weapon weight if equipped
+    if (this.character.equipment.mainHand) {
+      weight += this.character.equipment.mainHand.weight;
+    }
+    
+    // Add off hand item weight if equipped
+    if (this.character.equipment.offHand) {
+      weight += this.character.equipment.offHand.weight;
+    }
+    
+    // Add accessories weight
+    weight += this.character.equipment.accessories.reduce((total, item) => 
+      total + (item?.weight || 0), 0);
+    
+    return weight;
   }
 
   getTotalValue(): number {
-    return this.character.inventory.reduce((total, item) => 
+    const inventoryValue = this.character.inventory.reduce((total, item) => 
       total + ((item.value || 0) * item.quantity), 0);
+    
+    const equipmentValue = this.getEquipmentValue();
+    
+    return inventoryValue + equipmentValue;
+  }
+
+  private getEquipmentValue(): number {
+    let value = 0;
+    
+    if (this.character.equipment.armor) {
+      value += this.character.equipment.armor.value || 0;
+    }
+    
+    if (this.character.equipment.mainHand) {
+      value += this.character.equipment.mainHand.value || 0;
+    }
+    
+    if (this.character.equipment.offHand) {
+      value += this.character.equipment.offHand.value || 0;
+    }
+    
+    value += this.character.equipment.accessories.reduce((total, item) => 
+      total + (item?.value || 0), 0);
+    
+    return value;
   }
 }
